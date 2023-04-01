02abb0fbd132faa2b59e8b53bec4cec2b3f10da8
DigitalBridgeRDC <124746586+DigitalBridgeRDC@users.noreply.github.com>
2023-04-01 11:10:30 -0700

    Add files via upload

---
+++ notebooks/K-Means/k-means.py
@@ -0,0 +1,36 @@
+import numpy as np
+from sklearn.cluster import KMeans
+import pandas as pd
+
+# Load the dataset
+data = pd.read_csv('Wholesale customers data.csv')
+X = data.iloc[:, [2, 3, 4, 5, 6, 7]].values
+
+# Find the optimal number of clusters using the elbow method
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+    
+import matplotlib.pyplot as plt
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Apply K-Means clustering with k=3
+kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualize the clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Fresh')
+plt.ylabel('Milk')
+plt.legend()
+plt.show()
